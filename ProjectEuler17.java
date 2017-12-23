ae4e408607e608a9853e4f07b7764b21539a4aa3
softProcess <34180490+softProcess@users.noreply.github.com>
2017-12-22 21:24:11 -0600

    Add files via upload

---
+++ ProjectEuler17.java
@@ -0,0 +1,49 @@
+
+package projecteuler17;
+
+
+public class ProjectEuler17 {
+
+   
+    public static void main(String[] args) {
+      /*
+        
+                              TEN-3
+        ONE- 3                AND- 3
+        TWO- 3     TWENTY- 6  THOUSAND- 8
+        THREE- 5   THIRTY- 6
+        FOUR- 4    FORTY- 5
+        FIVE- 4    FIFTY- 5
+        SIX - 3    SIXTY- 5
+        SEVEN- 5   SEVENTY- 7
+        EIGHT- 5   EIGHTY- 6
+        NINE- 4    HUNDRED- 6
+        NINETY- 5  -----------
+        --------   TOTAL- 46
+        TOTAL- 36          
+        
+        
+        ELEVEN- 6
+        TWELVE- 6
+        THIRTEEN- 8
+        FOURTEEN- 8
+        FIFTEEN- 7
+        SIXTEEN- 7
+        SEVENTEEN- 9
+        EIGHTEEN- 8
+        NINETEEN- 8
+        ---------------
+        TOTAL- 67
+        
+   0-99 -     36+ TEN+ 67+ 10*46+8*36 = 854
+   100-999 - 100*36+63+9*854+9*99*10 = 20259
+   1000- 11       
+        
+   TOTAL - 21124     
+        
+        */
+        
+                
+    }
+    
+}
